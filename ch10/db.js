76777ffc384e993d323a208dfb5b961fefcc14ec
Masatoshi Nishimura <massa_n@yahoo.ca>
2015-05-27 12:17:59 -0400

    finalized

--- ch10/db.js
+++ ch10/db.js
@@ -1,8 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/social', function (err) {
-	if (err) return new Error(err);
-	console.log('mongodb connected');
-})
-
+var url = proces.env.MONGOLAB_URI || 'mongodb://localhost/social'
+mongoose.connect(url)
 
 module.exports = mongoose;
